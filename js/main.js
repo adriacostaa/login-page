46b5c41780382ff4aa1de0131871c4f3ca39e9b1
adriaaCosta <adria.c@outlook.com>
2020-05-01 19:25:42 -0400

    animacao de desfoco

--- js/main.js
+++ js/main.js
@@ -5,7 +5,14 @@ function focusFunc(){
     parent.classList.add('focus');
 }
 
+function blurFunc(){
+    let parent = this.parentNode.parentNode;
+    if (this.value == ""){
+        parent.classList.remove('focus');
+    }
+}
+
 inputs.forEach(input => {
     input.addEventListener('focus', focusFunc);
-    
+    input.addEventListener('blur', blurFunc);
 });
