e02480ff986940caa10c5e4a1b9ae99241f5ca0f
adriaaCosta <adria.c@outlook.com>
2020-05-01 18:52:21 -0400

    estilização foco formulario

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const inputs = document.querySelector('.input');
+
+// function focusFunc(){
+//     let parent = this.parentNode.parentNode;
+//     parent.classList.add('focus');
+// }
+
+// inputs.forEach(input => {
+//     input.addEventListener('focus', focusFunc);
+// });
